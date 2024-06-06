9724705fe5b7e8ffabf3a77f95e31fc5c0c16289
Ilesh Jain <ilesh.jain@kellogg.northwestern.edu>
2024-06-06 03:02:12 +0000

    update

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,20 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry.uploaded_image.attach(params["uploaded_image"])
+      @entry["user_id"] = session["user_id"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    else
+      flash["notice"] = "Login first."
+    end
   end
 
 end
